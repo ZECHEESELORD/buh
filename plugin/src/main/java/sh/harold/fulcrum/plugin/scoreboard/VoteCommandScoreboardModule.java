aaadb4a3ad08a4fcfdcce58cffd3f4a124bf3cd1
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2025-11-24 02:17:08 -0500

    preoperly add now fixed to the /vote prompt

--- plugin/src/main/java/sh/harold/fulcrum/plugin/scoreboard/VoteCommandScoreboardModule.java
+++ plugin/src/main/java/sh/harold/fulcrum/plugin/scoreboard/VoteCommandScoreboardModule.java
@@ -16,6 +16,6 @@ public final class VoteCommandScoreboardModule implements ScoreboardModule {
 
     @Override
     public List<String> renderLines(Player player) {
-        return List.of("&b&l/vote");
+        return List.of("&b&l/vote &f(NOW FIXED!)");
     }
 }
