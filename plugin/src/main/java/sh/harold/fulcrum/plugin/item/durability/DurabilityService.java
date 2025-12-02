e2777c3e174987d743c35aaa0cd57db602777a28
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2025-12-02 01:17:06 -0500

    chore(item): personalize defunct message per category

--- plugin/src/main/java/sh/harold/fulcrum/plugin/item/durability/DurabilityService.java
+++ plugin/src/main/java/sh/harold/fulcrum/plugin/item/durability/DurabilityService.java
@@ -54,9 +54,27 @@ public final class DurabilityService implements Listener {
             if (updated.defunct()) {
                 event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 0.5f);
                 event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 1.0f, 1.0f);
-                event.getPlayer().sendMessage(net.kyori.adventure.text.Component.text("Your item has lost all its durability, but it's not broken! Repair it to continue use!", net.kyori.adventure.text.format.NamedTextColor.RED));
+                String label = label(instance.definition().category());
+                event.getPlayer().sendMessage(net.kyori.adventure.text.Component.text("Your " + label + " has lost all its durability, but it's not broken! Repair it to continue use!", net.kyori.adventure.text.format.NamedTextColor.RED));
             }
         }));
+    }
+
+    private String label(sh.harold.fulcrum.plugin.item.model.ItemCategory category) {
+        return switch (category) {
+            case HELMET -> "helmet";
+            case CHESTPLATE -> "chestplate";
+            case LEGGINGS -> "leggings";
+            case BOOTS -> "boots";
+            case SWORD -> "sword";
+            case AXE -> "axe";
+            case BOW -> "bow";
+            case TRIDENT -> "trident";
+            case SHOVEL -> "shovel";
+            case HOE -> "hoe";
+            case PICKAXE -> "pickaxe";
+            default -> "item";
+        };
     }
 
     private int applyUnbreaking(int damage, int level) {
